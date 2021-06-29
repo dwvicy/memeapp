c073cf3fb52ef5ac62b1e4c69d6b9c41d033f3e6
Vaishnavi <vaishdw2207@gmail.com>
2021-06-29 10:44:18 +0530

    feat: addMeme

--- lib/screens/feature.dart
+++ lib/screens/feature.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:line_icons/line_icon.dart';
+import 'package:line_icons/line_icons.dart';
 
 class FeaturePage extends StatefulWidget {
   const FeaturePage({Key? key}) : super(key: key);
@@ -12,15 +13,41 @@ class _FeaturePageState extends State<FeaturePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.red,
+      backgroundColor: Colors.white,
       appBar: AppBar(
+        backgroundColor: Colors.white,
+      elevation: 0,
         leading: IconButton(
           icon: LineIcon.arrowLeft(),
           onPressed: () {
             Navigator.pop(context);
           },
         ),
+        title: Text('Add Meme', style: TextStyle(color: Colors.black54, fontSize: 25),),
       ),
+      body: Center(child: Padding(
+        padding: const EdgeInsets.only(left: 20, right: 20),
+        child: MaterialButton(
+          height: 50,
+          minWidth: 20,
+          color: Colors.pink,
+          onPressed: () {}, child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Icon(
+          LineIcons.upload,
+          color: Colors.white,
+          size: 24.0,
+    ),
+        ),
+    Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Text('Upload your meme', style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 25),),
+    )
+        ],),),
+      ),),
     );
   }
 }
